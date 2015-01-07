da6307e7bfd83feef6f272a167872819e9ae1b5b
Akash Patel <akash@Akashs-MacBook-Pro-3.local>
2015-01-07 15:09:53 +0100

    test file to see the workings of particleistribution-m

--- test_akash.m
+++ test_akash.m
@@ -1,7 +1,12 @@
-coordinates = [1,1; 2,1; 3,2; 3,6; 6,8; 6,6; 5,2; 7,6;];
-connections = [1,2; 2,3; 3,4; 4,5; 5,6; 6,7; 6,8];
 
-particleDistribution(coordinates, connections);
+
+map = loadMap;
+l = map.getMapLines;
+
+particleDistribution(map , 100)
+
+
+% particleDistribution(map, connections, coordinates, 100);
     
 %weightCalculation([0 0], [6 6], [-90 0])
     
