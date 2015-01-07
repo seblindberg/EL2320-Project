3eff6d3a2a6e27b2a9964140e63e8b0001830280
Akash Patel <akash@Akashs-MacBook-Pro-3.local>
2015-01-07 14:48:33 +0100

    particledistribution.m adjusted to work with MapLines. Returns vector of particle initial positions.

--- particleDistribution.m
+++ particleDistribution.m
@@ -1,18 +1,25 @@
-function [ particles ] = particleDistribution( map, connections,M)
+function [ particlePosition ] = particleDistribution( map, M)
 
 
-dist = zeros(size(connections,1),1);
-for i = 1:size(connections,1);
-    dist(i,:) = pdist([map(connections(i,1),:) ; map(connections(i,2),:)],'euclidean');
+lines = map.getMapLines;
+dist = zeros(1, size(lines,2));
+for i = 1:size(dist,2);
+    dist(:,i) = lines(:,i).length;
 end
 
+ 
+% dist = zeros(size(connections,1),1);
+% for i = 1:size(connections,1);
+%     dist(i,:) = pdist([map(connections(i,1),:) ; map(connections(i,2),:)],'euclidean');
+% end
+% 
 % dist
 % connections
 
 cdf = cumsum(dist);
 max_cdf = max(cdf);
 cdf = cdf/max_cdf;
-%Particle population size
+
 S = zeros(2,M);
 cumulative_sum = zeros(1,M);
 
@@ -21,7 +28,7 @@ r_0 = rand / M;
 % length.
 for m = 1 : M
     i = find(cdf >= r_0,1,'first');
-    S(:,m) = [i dist(i,1)];
+    S(:,m) = [i dist(1,i)];
     cumulative_sum(1,m) = r_0;
     
     r_0 = r_0 + 1/M;
@@ -29,30 +36,45 @@ for m = 1 : M
 end
 
 %need to add a 1 to the end or we will miss the last line
-changeInDist = [logical(diff(S(2,:))) 1];
+changeOfLine = [logical(diff(S(1,:))) 1];
 norm_prob = zeros(1,M);
 min_cdf = 0;
-max_cdf = cdf
+max_cdf = cdf;
 k = 1;
 l = 1;
+d = 1;
+particlePosition = zeros(2, M);
+
+
 for i = 1:M
-    if (changeInDist(i) == 1)
-        norm_prob(1,k:i) = ( cumulative_sum(1,k:i) - min_cdf ) / (max_cdf(l) - min_cdf);
-        k = i+1;
+    if (changeOfLine(i) == 1)
+        normalizedProbTemp = ( cumulative_sum(1,k:i) - min_cdf ) / (max_cdf(l) - min_cdf);
+        norm_prob(1,k:i) = normalizedProbTemp;
         %assign current max cdf value to next min value.
         min_cdf = max_cdf(l);
-        l = l + 1;
+        
+        %Calculate the position of each generated particle
+        for j = 1:size(normalizedProbTemp,2)
+           particlePosition(:,d) = lines(1,S(1,i)) * normalizedProbTemp(1,j);
+           d = d+1;
+        end
+        
+        k = i+1;
+        l = l + 1; 
+
+
     end
     
-        
-%     else
-%         norm_prob(1,k+1:i) = (cumulative_sum(1,k+1:i) - cumulative_sum(1,k+1) )/ (cumulative_sum(1,i) - cumulative_sum(1,k+1));
-%     end
 end
 
-
-[S' norm_prob' cumulative_sum' changeInDist']
-
+%Data print which shows the following:
+% [Connection number
+% Dist of Connection 
+% Distance percentage of line where particle is to be place
+% Particle PosX
+% Particle PosY
+% 1 when a we are about to switch to another line]
+[S' norm_prob' particlePosition' changeOfLine'];
 
 
 
